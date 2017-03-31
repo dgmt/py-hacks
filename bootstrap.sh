3206dd0263c8f70993c767b731935b124c4f5fa9
Vladimir Riznic <vladimir.riznic@tamedia.ch>
2017-03-31 14:08:30 +0200

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get -y upgrade python
+sudo apt-get -y install --only-upgrade python
 
 # install python
 sudo apt-get -y install git python-dev python-pip
@@ -11,5 +11,6 @@ cd /code/src
 # pip install -r requirements.txt
 pip install pymongo
 pip install requests
+pip install requests[security]
 pip install ujson
 pip install python-telegram-bot
